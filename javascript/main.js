cd81d25ae6acced99fe6400d60500a8b6caad152
Saiful Nakeusa <snakeusa8@gmail.com>
2020-08-02 14:10:04 +0700

    finish scroll to top

--- javascript/main.js
+++ javascript/main.js
@@ -4,3 +4,26 @@ window.addEventListener("scroll", () => {
     navbar.classList.toggle("change-nav", window.scrollY > 0)
 })
 
+// -----------
+
+const scrolltoTop = document.getElementById("scroll-to-top");
+let dataShow = false;
+
+window.addEventListener("scroll", () => {
+    if(window.scrollY > 50 && !dataShow){
+        scrolltoTop.setAttribute("data-show", "true");
+        dataShow = true;
+    }
+
+    if(window.scrollY <= 50 && dataShow){
+        scrolltoTop.setAttribute("data-show", "false");
+        dataShow = false;
+    }
+} )
+
+scrolltoTop.addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
+})
